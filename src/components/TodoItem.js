d0034e09e4749be032e358e13e250c2ca729b9b6
dewsy <you@example.com>
2019-03-25 14:03:31 +0100

    finished todos are now crossed through

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
 
 class TodoItem extends Component {
+
+    getStyle = () => {
+            return {
+                textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+            }
+        }
+
     render() {
         return (
-            <div>
+            <div style={this.getStyle()}>
                 <h3> { this.props.todo.title } </h3>
             </div>
         );
     }
+}
+
+//PropTypes
+TodoItem.propTypes= {
+    todo: PropTypes.object.isRequired
 }
 
 export default TodoItem;
