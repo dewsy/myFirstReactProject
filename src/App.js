283a47ad527d717aa0f3e3eb042e004ce74ab285
dewsy <you@example.com>
2019-03-25 13:26:47 +0100

    first components added, basic state transferred

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos.js'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    todos : [
+      {
+        id: 1,
+        title: 'title',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'title2',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'title3',
+        completed: false
+      }
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
-        <h1>app</h1>
+        <Todos todos = {this.state.todos} />
       </div>
     );
   }
