2fa893fbe01078c28b6f4704a6d30e7e7d75dfd6
Dewsy <dewsyy@gmail.com>
2019-03-27 13:21:11 +0100

    fake backend got wired in

--- src/App.js
+++ src/App.js
@@ -3,35 +3,24 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import Header from './components/layout/Header';
 import Todos from './components/Todos.js'
-import AddTodos from './components/AddTodo.js'
 import './App.css';
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
+import axios from 'axios';
 
 
 class App extends Component {
 
   state = {
-    todos : [
-      {
-        id: 1,
-        title: 'title',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'title2',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'title3',
-        completed: false
-      }
-    ]
+    todos : []
   };
 
-  //toggles complete
+  componentDidMount() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+          .then(res => this.setState({todos: res.data}))
+  }
+
+    //toggles complete
   markComplete = (id) => {
     this.setState({todos: this.state.todos.map(todo => {
       if(todo.id === id) {
@@ -46,12 +35,9 @@ class App extends Component {
       })};
 
   addTodo = (title) => {
-    const newTodo = {
-      id: 4,
-      title: title,
-      completed: false
-    };
-    this.setState({todos: [...this.state.todos, newTodo]})
+      axios.post("https://jsonplaceholder.typicode.com/todos", {title, completed: false})
+          .then(res => this.setState({todos: [...this.state.todos, res.data]}) );
+
 
   };
 
